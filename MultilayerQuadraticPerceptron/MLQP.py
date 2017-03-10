bc8366d6b6b4edd6f6ceb854a69c2850b39849c2
Liu Chang <lc3018121@sjtu.edu.cn>
2017-03-10 13:14:20 +0800

    Finish the basic MLQP part

---
+++ MultilayerQuadraticPerceptron/MLQP.py
@@ -0,0 +1,128 @@
+import random
+import numpy as np
+
+class MLQP():
+	""" It's similar with a tradition backprop algoritm, except that
+	the activation function is y_j = sum_i(u_j * (y_i^2) + v_j * y_i) + b_j
+	"""
+	def __init__(self, sizes):
+		""" The network consists of len(sizes) layers, 
+		each of which contains sizes[i] neurons."""
+		self.num_layers = len(sizes)
+		self.sizes = sizes
+		self.u = [np.random.randn(j,i) for i, j in zip(sizes[:-1],sizes[1:])]
+		self.v = [np.random.randn(j,i) for i, j in zip(sizes[:-1],sizes[1:])]
+		self.b = [np.random.randn(x,1) for x in sizes[1:]]
+
+	def feedforward(self, x):
+		""" Return the output when input is equal to x"""
+		for u, v, b in zip(self.u, self.v, self.b):
+			x = sigmoid(np.dot(u,x**2) + np.dot(v,x) + b)
+		return x
+
+	def SGD_online(self, training_data, eta):
+		""" on-line mode, update weights every single input."""
+		itr = 0
+		while True:
+			itr = itr+1
+			random.shuffle(training_data)
+			error_total = self.update_online(training_data, eta)
+
+			# the MSE of output 
+			if error_total < 0.01:
+				print("Iteration times: {}".format(itr))
+				break
+
+	def update_online(self, training_data, eta):
+		error_total = 0
+		for x, y in training_data:
+			nabla_u, nabla_v, nabla_b, error_single = self.backprop(x, y)
+
+			self.u = [u-(eta*nu) for u, nu in zip(self.u, nabla_u)]
+			self.v = [v-(eta*nv) for v, nv in zip(self.v, nabla_v)]
+			self.b = [b-(eta*nb) for b, nb in zip(self.b, nabla_b)]
+			error_total = error_total+error_single
+
+		return error_total/len(training_data)
+
+	def SGD_batch(self, training_data, epochs, mini_batch_size, eta):
+		""" batch mode, update weights every epoch."""
+		num_data = len(training_data)
+		for itr in xrange(epochs):
+			random.shuffle(training_data)
+			mini_batches = [ training_data[k:k+mini_batch_size] \
+							for k in xrange(0,num_data,mini_batch_size)]
+			error_total = 0.0
+			for mini_batch in mini_batches:
+				error_total = error_total + self.update_mini_batch(mini_batch, eta)
+			
+			# the MSE of output 
+			if error_total/num_data < 0.01:
+				print("Iteration times: {}".format(itr))
+				break
+
+	def update_mini_batch(self, mini_batch, eta):
+		nabla_u = [np.zeros(u.shape) for u in self.u]
+		nabla_v = [np.zeros(v.shape) for v in self.v]
+		nabla_b = [np.zeros(b.shape) for b in self.b]
+
+		error_total = 0.0
+
+		for x, y in mini_batch:
+			delta_nabla_u, delta_nabla_v, delta_nabla_b, error_single = self.backprop(x, y)
+			nabla_u = [nu+dnu for nu, dnu in zip(nabla_u, delta_nabla_u)]
+			nabla_v = [nv+dnv for nv, dnv in zip(nabla_v, delta_nabla_v)]
+			nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+			error_total = error_total + error_single
+				
+		self.u = [u-(eta/len(mini_batch)*nu) for u, nu in zip(self.u, nabla_u)]
+		self.v = [v-(eta/len(mini_batch)*nv) for v, nv in zip(self.v, nabla_v)]
+		self.b = [b-(eta/len(mini_batch)*nb) for b, nb in zip(self.b, nabla_b)]
+
+		return error_total
+
+
+	def backprop(self, x, y):
+		nabla_u = [np.zeros(u.shape) for u in self.u]
+		nabla_v = [np.zeros(v.shape) for v in self.v]
+		nabla_b = [np.zeros(b.shape) for b in self.b]
+
+		# feedforward
+		activation = x
+		zs = []
+		activations = [x]
+		for u, v, b in zip(self.u, self.v, self.b):
+			z = np.dot(u,activation**2) + np.dot(v, activation) + b
+			zs.append(z)
+			activation = sigmoid(z)
+			activations.append(activation)
+		# backward pass
+		delta = (activations[-1]-y)*sigmoid_prime(zs[-1])
+		nabla_b[-1] = delta
+		nabla_u[-1] = np.dot(delta, (activations[-2]**2).transpose())
+		nabla_v[-1] = np.dot(delta, activations[-2].transpose())
+		for l in xrange(2, self.num_layers):
+			z = zs[-l]
+			sp = sigmoid_prime(z)
+			
+			delta = np.dot(((2*self.u[-l+1]).transpose()*activations[-l]+self.v[-l+1].transpose()), delta) * sp
+			nabla_b[-l] = delta
+			nabla_u[-l] = np.dot(delta, (activations[-l-1]**2).transpose())
+			nabla_v[-l] = np.dot(delta, activations[-l-1].transpose())
+		
+		return (nabla_u, nabla_v, nabla_b, ((activations[-1]-y)**2).sum())
+	
+	def evaluate(self, test_data):
+		test_results = [(scalarize(self.feedforward(x)),y) \
+						for x,y in test_data]
+		num_success = sum(int(x==y) for (x,y) in test_results)
+		print("correct/total: {}/{} ({:.2f}%)".format(num_success,\
+			len(test_data),float(num_success)/len(test_data)*100))
+
+# utils
+def sigmoid(z):
+	return 1.0/(1.0+np.exp(-z))
+def sigmoid_prime(z):
+	return sigmoid(z)*(1-sigmoid(z))
+def scalarize(y):
+	return np.argmax(y)
---
+++ MultilayerQuadraticPerceptron/loader.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def load_data():
+    training_data = []
+    test_data = []
+    train_txt = open("two_spiral_train.txt", "r")
+    test_txt = open("two_spiral_test.txt", "r")
+    # the training_data is in the format as bellow: 
+    # ([x1,
+    #  x2],[y1.
+    #       y2])
+    for line in train_txt:
+        x0, x1, y = line.split()
+        x = np.array([float(x0), float(x1)]).reshape(2, 1)
+        y = vectorized_result(int(y))
+        training_data.append((x,y))
+    # the test_data is in the format as bellow: 
+    # ([x1,
+    #  x2],y)
+    for line in test_txt:
+        x0, x1, y = line.split()
+        x = np.array([float(x0), float(x1)]).reshape(2, 1)
+        y = int(y)
+        test_data.append((x,y))
+    train_txt.close()
+    test_txt.close()
+    return training_data, test_data
+
+def vectorized_result(j):
+    e = np.zeros((2,1))
+    e[j] = 1.0
+    return e
